e77a87a5198423bd417e0259469a183dffb3c4b1
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-27 00:41:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+
+  function togglePepperoni(e) {
+    setPepperoniIsChecked(e.target.checked);
+  }
+
   return (
-    <div></div>
-  )
+    <div className="pizza-container">
+      <h1>Select Pizza Toppings</h1>
+      
+      <div className="topping-option">
+        <input
+          type="checkbox"
+          id="pepperoni"
+          checked={pepperoniIsChecked}
+          aria-checked={pepperoniIsChecked}
+          onChange={togglePepperoni}
+        />
+        <label htmlFor="pepperoni">Add pepperoni</label>
+      </div>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniIsChecked ? <li className="pepperoni">Pepperoni</li> : null}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
